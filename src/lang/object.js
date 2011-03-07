6d1372b707ebc4c83be5660b25ae7e359dbd1e74
Joe Gornick <joe@joegornick.com>
2011-03-06 22:16:53 -0600

    fusejs: Add documentation for fuse.Object. [FuseJS LH #98] [Joe Gornick]

--- src/lang/object.js
+++ src/lang/object.js
@@ -10,10 +10,33 @@
      defaultPort = protocol == 'ftp:' ? 21 : protocol == 'https:' ? 443 : 80,
      toString = fuse._.toString;
 
+    /**
+     * Determines if the object is a HTMLElement (ELEMENT_NODE).
+     * 
+     * @methodOf fuse.Object
+     * @static
+     * @name isElement
+     * 
+     * @param {Object} value The object to inspect.
+     * 
+     * @returns {Boolean}
+     */
     function isElement(value) {
       return !!value && value.nodeType == 1;
     }
 
+    /**
+     * Determines if the object is empty. An object is empty when it doesn't 
+     * contain any elements.
+     * 
+     * @methodOf fuse.Object
+     * @static
+     * @name isEmpty
+     * 
+     * @param {Object} object The object to inspect.
+     * 
+     * @returns {Boolean}
+     */    
     function isEmpty(object) {
       var result = true;
       if (object) {
@@ -24,43 +47,136 @@
       return result;
     }
 
+    /**
+     * Determines if the object is a function.
+     * 
+     * @methodOf fuse.Object
+     * @static
+     * @name isFunction
+     * 
+     * @param {Object} value The object to inspect.
+     * 
+     * @returns {Boolean}
+     */    
     function isFunction(value) {
       return toString.call(value) == '[object Function]';
     }
 
+    /**
+     * Determines if the object is a fuse.Hash.
+     * 
+     * @methodOf fuse.Object
+     * @static
+     * @name isHash
+     * 
+     * @param {Object} value The object to inspect.
+     * 
+     * @returns {Boolean}
+     */        
     function isHash(value) {
       var Hash = isHash[ORIGIN].Hash;
       return !!value && value.constructor == Hash && value != Hash.prototype;
     }
 
-    // Host objects can return type values that are different from their actual
-    // data type. The objects we are concerned with usually return non-primitive
-    // types of object, function, or unknown.
+    /**
+     * Determines if the object is a host type.
+     * 
+     * Host objects can return type values that are different from their actual
+     * data type. The objects we are concerned with usually return non-primitive
+     * types of object, function, or unknown.
+     * 
+     * @methodOf fuse.Object
+     * @static
+     * @name isHostType
+     * 
+     * @param {Object} object The object to inspect.
+     * @param {String} property The property name to search.
+     * 
+     * @returns {Boolean}
+     */     
     function isHostType(object, property) {
       var type = typeof object[property];
       return type == 'object' ? !!object[property] : !NON_HOST_TYPES[type];
     }
 
+    /**
+     * Determines if the specified object is a number.
+     * 
+     * @methodOf fuse.Object
+     * @static
+     * @name isNumber
+     * 
+     * @param {Object} value The object to inspect.
+     * 
+     * @returns {Boolean}
+     */       
     function isNumber(value) {
       return toString.call(value) == '[object Number]' && isFinite(value);
     }
 
-    // ES5 4.3.2
+    /**
+     * Determines if the object is a primitive type.
+     * 
+     * @methodOf fuse.Object
+     * @static
+     * @name isPrimitive
+     * 
+     * @param {Object} value The object to inspect.
+     * 
+     * @returns {Boolean}
+     * 
+     * @see ES5 4.3.2
+     */
     function isPrimitive(value) {
       var type = typeof value;
       return value == null || type == 'boolean' || type == 'number' || type == 'string';
     }
 
+    /**
+     * Determines if the object is a regular expression.
+     * 
+     * @methodOf fuse.Object
+     * @static
+     * @name isRegExp
+     * 
+     * @param {Object} value The object to inspect.
+     * 
+     * @returns {Boolean}
+     */    
     function isRegExp(value) {
       return toString.call(value) == '[object RegExp]';
     }
 
+    /**
+     * Determines if the object is a string.
+     * 
+     * @methodOf fuse.Object
+     * @static
+     * @name isString
+     * 
+     * @param {Object} value The object to inspect.
+     * 
+     * @returns {Boolean}
+     */        
     function isString(value) {
       return toString.call(value) == '[object String]';
     }
 
-    // https://developer.mozilla.org/En/Same_origin_policy_for_JavaScript
-    // http://www.iana.org/assignments/port-numbers
+    /**
+     * Determines if the specified URL is of the same origin as our current
+     * location.
+     * 
+     * @methodOf fuse.Object
+     * @static
+     * @name isSameOrigin
+     * 
+     * @param {String} url The URL to inspect.
+     * 
+     * @returns {Boolean}
+     * 
+     * @see https://developer.mozilla.org/En/Same_origin_policy_for_JavaScript
+     * @see http://www.iana.org/assignments/port-numbers
+     */         
     function isSameOrigin(url) {
       var domainIndex, urlDomain, docDomain = fuse._doc.domain,
        parts = String(url).match(reUrlParts), result = true;
@@ -77,6 +193,19 @@
 
     /*------------------------------------------------------------------------*/
 
+    /**
+     * Determines if the specified object is an array.
+     * 
+     * @methodOf fuse.Object
+     * @static
+     * @name isArray
+     * 
+     * @param {Object} value The object to inspect.
+     * 
+     * @returns {Boolean}
+     * 
+     * @see fuse.Array.isArray
+     */    
     Object.isArray = fuse.Array.isArray;
     Object.isElement = isElement;
     Object.isEmpty = isEmpty;
@@ -96,6 +225,18 @@
 
   (function(Object) {
 
+    /**
+     * Returns a shallow clone of the object.
+     * 
+     * @methodOf fuse.Object
+     * @static
+     * @name clone
+     * 
+     * @param {Object} object The object to clone.
+     * @param {Boolean} deep Flag to perform a deep clone.
+     * 
+     * @returns {fuse.Object}
+     */    
     function clone(object, deep) {
       var length, result, constructor, i = -1;
       if (object) {
@@ -143,25 +284,78 @@
       return Object();
     }
 
+    /**
+     * Copies all properties from the source object to the destination object.
+     * 
+     * This method does directly modify the destination object and doesn't 
+     * return a new object.
+     * 
+     * @methodOf fuse.Object
+     * @static
+     * @name extend
+     * 
+     * @param {Object} destination The object to receive the source properties.
+     * @param {Object} source The object to copy the properties from.
+     * 
+     * @returns {Object} Returns the destination object with source properties
+     *   added/merged to it.
+     */ 
     function extend(destination, source) {
       Object.each(source, function(value, key) { destination[key] = value; });
       return destination;
     }
 
-    // ES5 15.2.4.2
-    // The erratum states Object#toString should return
-    // [object Null] and [object Undefined] for null and undefined values.
-    // However, Null and Undefined are *not* [[Class]] property values.
+    /**
+     * Returns the class name of the object.
+     * 
+     * The erratum states Object#toString should return [object Null] and 
+     * [object Undefined] for null and undefined values. However, Null and 
+     * Undefined are *not* [[Class]] property values.
+     * 
+     * @methodOf fuse.Object
+     * @static
+     * @name getClassOf
+     * 
+     * @param {Object} object The object to get the class name.
+     * 
+     * @returns {String}
+     * 
+     * @see ES5 15.2.4.2
+     */     
     function getClassOf(object) {
       if (object == null) throw new TypeError;
       return getClassOf[ORIGIN].String(Object.prototype.toString.call(object).slice(8, -1));
     }
 
+    /**
+     * Returns an array containing the keys from the object.
+     * 
+     * @methodOf fuse.Object
+     * @static
+     * @name keys
+     * 
+     * @param {Object} object The object to get the keys from.
+     * 
+     * @returns {fuse.Array}
+     * 
+     * @see ES5 15.2.3.14
+     */
     function keys(object) {
       var result = keys.raw.call(object);
       return result.constructor == fuse.Array ? result : Array.fromArray(result);
     }
 
+    /**
+     * Returns an array containing the values from the object.
+     * 
+     * @methodOf fuse.Object
+     * @static
+     * @name values
+     * 
+     * @param {Object} object The object to get the values from.
+     * 
+     * @returns {fuse.Array}
+     */    
     function values(object) {
       var result = values[ORIGIN].Array(), i = -1;
       if (Object.isPrimitive(object)) {
@@ -171,6 +365,17 @@
       return result;
     }
 
+    /**
+     * Returns an HTML representation of the object.
+     * 
+     * @methodOf fuse.Object
+     * @static
+     * @name toHTML
+     * 
+     * @param {Object} object The object to transform to HTML.
+     * 
+     * @returns {fuse.String}
+     */
     function toHTML(object) {
       var String = toHTML[ORIGIN].String;
       return object && typeof object.toHTML == 'function'
@@ -212,8 +417,23 @@
 
   /*--------------------------------------------------------------------------*/
 
-  // Use fuse.Object.hasKey() on object Objects only as it may error on DOM Classes
-  // https://bugzilla.mozilla.org/show_bug.cgi?id=375344
+  /**
+   * Determines if the object has the specified property name.
+   * 
+   * Use fuse.Object.hasKey() on object Objects only as it may error on DOM 
+   * Classes (https://bugzilla.mozilla.org/show_bug.cgi?id=375344)
+   * 
+   * @methodOf fuse.Object
+   * @static
+   * @name hasKey
+   * 
+   * @param {Object} object The object to inspect.
+   * @param {String} property The property name to search.
+   * 
+   * @returns {Boolean}
+   * 
+   * @see ES5 15.2.4.5
+   */
   fuse.Object.hasKey = (function() {
 
     var objProto = Object.prototype,
@@ -269,6 +489,20 @@
 
   /*--------------------------------------------------------------------------*/
 
+  /**
+   * Calls a function for each element in the object.
+   * 
+   * @methodOf fuse.Object
+   * @static
+   * @name each
+   * 
+   * @param {Function} callback Function to execute for each element.  The
+   *   callback is passed 3 arguments: element value, element key and 
+   *   the object.  If callback returns {Boolean} false, the loop will stop.
+   * @param {Object} [thisArg] Object to use as this when executing callback.
+   * 
+   * @returns {Object}
+   */  
   fuse.Object.each = (function() {
 
     var each, isFunction = fuse.Object.isFunction, hasKey = fuse.Object.hasKey,
